4b02dd4aa1748da1a6394f1941f60f24c993ea2e
juniorcintra <junior.developer@outlook.com.br>
2020-07-06 01:28:00 -0300

    first commit

---
+++ src/components/Menu/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%,
+background: transparent
+display: flex;
+justify-content: flex-end;
+align-items: flex-end;
+`;
+
+export const Content = styled.div`
+  width: 40%;
+  display: flex;
+  fllex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
